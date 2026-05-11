46c38a62451a3104c3996c838699c3fce53661ed
Lucas Neo <lucasneo@outlook.com.br>
2026-05-11 19:37:37 -0300

    Feat: Add path /jogos

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
+let jogos = []
+let nextId = 1
+
+app.get('/jogos', (req, res) => {
+  res.json(jogos)
+})
+
+app.post('/jogos', (req, res) => {
+  const { nome, tipo, nota, review } = req.body
+
+  const novoJogo = {
+    id: nextId++,
+    nome,
+    tipo,
+    nota,
+    review
+  }
+
+  jogos.push(novoJogo)
+  res.status(201).json(novoJogo)
+})
+
 app.get('/', (req, res) =>{
     res.status(200).json({ status: "ok", message: "Server Workiing"})
     console.log(req.url)
